0e2ae4a8b29506dca236f8a621f2c697803729a6
Mohammad Kianpisheh <mkian1368@gmail.com>
2021-09-30 04:01:43 -0400

    task 1 added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,82 +3,92 @@ import React from "react";
 import "video.js/dist/video-js.css";
 
 import Task2Page from "./pages/Task2Page";
-import VideoPlayer from "./components/VideoPlayer";
-import DescriptionPane from "./components/DescriptionPane";
-import Login from "./components/Login";
-import TaskDescription from "./components/TaskDescription";
-import StepProposition from "./components/StepProposition";
-import PropositionsPane from "./components/PropositionsPane";
+import Task1Page from "./pages/Task1Page";
 
 import "./App.css";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { videoId: "", token: "" };
-        this.user = "";
+	constructor(props) {
+		super(props);
+		this.state = { videoId: "", token: "" };
+		this.user = "";
+		this.currentPage = "task1";
 
-        this.setToken = this.setToken.bind(this);
-        this.getToken = this.getToken.bind(this);
-        this.handleVideoPlay = this.handleVideoPlay.bind(this);
-        this.handleNavifationButtonClick = this.handleNavifationButtonClick.bind(this);
-    }
+		this.setToken = this.setToken.bind(this);
+		this.getToken = this.getToken.bind(this);
+		this.handleVideoPlay = this.handleVideoPlay.bind(this);
+		this.handleNavifationButtonClick = this.handleNavifationButtonClick.bind(this);
+	}
 
-    render() {
-        const token = this.getToken();
+	render() {
+		const token = this.getToken();
 
-        // if (!token) {
-        //     return <Login setToken={this.setToken} />;
-        // }
+		// if (!token) {
+		//     return <Login setToken={this.setToken} />;
+		// }
 
-        return (
-            <Task2Page
-                handleVideoPlay={this.handleVideoPlay}
-                handleNavifationButtonClick={this.handleNavifationButtonClick}
-            ></Task2Page>
-        );
-    }
+		let page = (
+			<Task1Page
+				user={this.user}
+				videoId={this.state.videoId}
+				handleVideoPlay={this.handleVideoPlay}
+				handleNavifationButtonClick={
+					this.handleNavifationButtonClick
+				}></Task1Page>
+		);
+		if (this.currentPage === "task2") {
+			page = (
+				<Task2Page
+					handleVideoPlay={this.handleVideoPlay}
+					handleNavifationButtonClick={
+						this.handleNavifationButtonClick
+					}></Task2Page>
+			);
+		}
 
-    handleNavifationButtonClick(btn) {
-        console.log(btn);
-    }
+		return <React.Fragment>{page}</React.Fragment>;
+	}
 
-    handleVideoPlay(videoUrl) {
-        // remove time from the url
-        let newUrl = videoUrl;
-        if (videoUrl.includes("&t=")) {
-            const urlFirstHalf = videoUrl.split("&t=")[0];
-            let urlSecondHalf = videoUrl.split("&t=")[1];
-            if (urlSecondHalf.includes("&v=")) {
-                urlSecondHalf = "&v=" + urlSecondHalf.split("&v=")[1];
-            }
-            newUrl = urlFirstHalf + urlSecondHalf;
-        }
+	handleNavifationButtonClick(btn) {
+		console.log(btn);
+	}
 
-        if (this.state.videoId !== newUrl) {
-            this.setState({ videoId: newUrl });
-        }
-    }
+	handleVideoPlay(videoUrl) {
+		// remove time from the url
+		let newUrl = videoUrl;
+		if (videoUrl.includes("&t=")) {
+			const urlFirstHalf = videoUrl.split("&t=")[0];
+			let urlSecondHalf = videoUrl.split("&t=")[1];
+			if (urlSecondHalf.includes("&v=")) {
+				urlSecondHalf = "&v=" + urlSecondHalf.split("&v=")[1];
+			}
+			newUrl = urlFirstHalf + urlSecondHalf;
+		}
 
-    setToken(userToken, username) {
-        sessionStorage.setItem("token", JSON.stringify(userToken));
-        this.user = username;
-        sessionStorage.setItem("user", this.user);
-        this.forceUpdate();
-    }
+		if (this.state.videoId !== newUrl) {
+			this.setState({ videoId: newUrl });
+		}
+	}
 
-    getToken() {
-        const tokenString = sessionStorage.getItem("token");
-        const userToken = JSON.parse(tokenString);
-        this.user = sessionStorage.getItem("user");
-        return userToken?.token;
-    }
+	setToken(userToken, username) {
+		sessionStorage.setItem("token", JSON.stringify(userToken));
+		this.user = username;
+		sessionStorage.setItem("user", this.user);
+		this.forceUpdate();
+	}
 
-    callAPI() {
-        fetch("http://localhost:9000/testAPI")
-            .then((res) => res.text())
-            .then((res) => this.setState({ apiResponse: res }));
-    }
+	getToken() {
+		const tokenString = sessionStorage.getItem("token");
+		const userToken = JSON.parse(tokenString);
+		this.user = sessionStorage.getItem("user");
+		return userToken?.token;
+	}
+
+	callAPI() {
+		fetch("http://localhost:9000/testAPI")
+			.then((res) => res.text())
+			.then((res) => this.setState({ apiResponse: res }));
+	}
 }
 
 export default App;
--- frontend/src/pages/Task1Page.js
+++ frontend/src/pages/Task1Page.js
@@ -1,24 +1,51 @@
-import VideoPlayer from "../components/VideoPlayer"
-import DescriptionPane from "../components/DescriptionPane"
+import "./Task1Page.css";
 
+import VideoPlayer from "../components/VideoPlayer";
+import DescriptionPane from "../components/DescriptionPane";
+import TaskDescription from "../components/TaskDescription";
+import TaskNavigation from "../components/TaskNavigation";
 
-export default function Task1Page() {
-    return (
-        <div id="main-container">
-            <div id="player-container">
-                <TaskDescription id="task-description-1" description={taskDescription1} />
-                <VideoPlayer
-                    id="video-player"
-                    width="600px"
-                    height="350px"
-                    onVideoPlay={this.handleVideoPlay}
-                />
-            </div>
-            <DescriptionPane
-                id="description-pane"
-                videoID={this.state.videoId}
-                user={this.user}
-            />
-        </div>
-    );
+export default function Task1Page(props) {
+	const taskDescription =
+		"Please describe the activity which is performed in the video in terms of the steps that are taken by the actor to acomplish the activity?";
+
+	return (
+		<div id="main-container">
+			<h1 style={{ flexGrow: 2 }}>Task 1</h1>
+			<div id="left-and-right-panes-container">
+				<div id="player-container">
+					<div id="left-pane-div">
+						<div id="task-description-div">
+							<TaskDescription
+								id="task-description-1"
+								description={taskDescription}
+							/>
+						</div>
+						<div id="video-player-div">
+							<VideoPlayer
+								id="video-player"
+								width="600px"
+								height="350px"
+								onVideoPlay={props.handleVideoPlay}
+							/>
+						</div>
+					</div>
+				</div>
+				<div id="right-pane-div">
+					<h5 style={{ marginBottom: "3px" }}>Please type here</h5>
+					<DescriptionPane
+						id="description-pane"
+						videoID={props.videoId}
+						user={props.user}
+					/>
+				</div>
+			</div>
+			<div id="task-navigation-div">
+				<TaskNavigation
+					handleNavifationButtonClick={
+						props.handleNavifationButtonClick
+					}></TaskNavigation>
+			</div>
+		</div>
+	);
 }
